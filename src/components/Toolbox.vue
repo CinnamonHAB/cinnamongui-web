eeab21e76b907dcec1c7a9d740f3d2a34138dcd2
kokolada <aanicic183@gmail.com>
2016-12-22 14:42:09 +0100

    made toolbox div scrollable vertically

--- src/components/Toolbox.vue
+++ src/components/Toolbox.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class='col-xs-2 full-height' style="background-color:#a0a0a0">
+  <div class='col-xs-2 full-height' id="toolbox-col" style="background-color:#a0a0a0">
     <p>Hello, I'm Toolbox</p>
   </div>
 </template>
+
+<style>
+#toolbox-col{
+	overflow-y: auto;
+}
+</style>
