0ce4e9c2e550cab153772192306c39cb45391b7e
Mak Krnic <mak@makkrnic.com>
2016-12-22 03:28:33 +0100

    add fabricjs
    Signed-off-by: Mak Krnic <mak@makkrnic.com>

--- src/components/ChCanvas.vue
+++ src/components/ChCanvas.vue
@@ -1,5 +1,24 @@
 <template>
   <div class='col-xs-8 full-height' style='background-color:#c0c0c0'>
-    <p>Hello, I'm ChCanvas<p>
+    <canvas id='main-canvas'></canvas>
   </div>
 </template>
+<!--
+<script>
+// var rect = new fabric.Rect({
+//   left: 100,
+//   top: 100,
+//   fill: 'red',
+//   width: 20,
+//   height: 20
+// });
+// canvas.add(rect);
+</script>
+-->
+
+<style>
+#main-canvas {
+  width: 100%;
+  height: 100%;
+}
+</style>
