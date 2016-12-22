d64e572e70917aa61cbb046b77230d1e1f94977f
Mak Krnic <mak@makkrnic.com>
2016-12-22 14:52:06 +0100

    add dummy toolbox button
    Signed-off-by: Mak Krnic <mak@makkrnic.com>

--- src/components/Toolbox.vue
+++ src/components/Toolbox.vue
@@ -1,8 +1,23 @@
 <template>
   <div class='col-xs-2 full-height' id="toolbox-col" style="background-color:#a0a0a0">
     <p>Hello, I'm Toolbox</p>
+    <ul>
+      <li>
+        <button v-on:click='addSensor'>Add a sensor</button>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    addSensor: function () {
+      window.alert('magic')
+    }
+  }
+}
+</script>
 
 <style>
 #toolbox-col{
